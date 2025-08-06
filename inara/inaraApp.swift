32aaac9929a4204027f8cc22b20400c19e421720
Oscar <ovonhauske@gmail.com>
2025-08-05 21:18:26 -0400

    Initial Commit

---
+++ inara/inaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  inaraApp.swift
+//  inara
+//
+//  Created by Oscar von Hauske on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct inaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
