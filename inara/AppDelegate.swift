a2c5f3da7dcfd3321c00daf51ab7bf20a515fb7b
ovonhauske <ovonhauske@users.noreply.github.com>
2026-01-02 19:23:32 -0600

    Added Firebase

---
+++ inara/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  inara
+//
+//  Created by Assistant on 1/2/26.
+//
+
+import UIKit
+import FirebaseCore
+
+@MainActor
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Configure Firebase if it hasn't been configured yet
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+        return true
+    }
+}
---
+++ inara/Model/AuthViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewController.swift
+//  inara
+//
+//  Created by Oscar von Hauske on 1/1/26.
+//
+
+import Foundation
