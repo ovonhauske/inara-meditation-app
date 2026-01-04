387950e54023faed05bff063b716e81840e1cbe1
ovonhauske <ovonhauske@users.noreply.github.com>
2026-01-04 12:24:54 -0600

    added button logic to show and hide icons and text

---
+++ inara/Views/File.swift
@@ -0,0 +1,1 @@
+                AppColors.surface.ignoresSafeArea()
---
+++ inara/Views/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  inara
+//
+//  Created by Oscar von Hauske on 1/4/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
